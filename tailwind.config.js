90f8f279392c5379c17f7e561c0947993ddc20de
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2024-01-22 11:37:54 +0000

    Add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,23 @@ export default {
         tablet: "768px",
         mobile: "375px",
       },
+      colors: {
+        orange: {
+          900: "#D87D4A",
+          800: "#fbaf85",
+        },
+        black: {
+          900: "#000000",
+          800: "#101010",
+        },
+        grey: {
+          900: "#F1F1F1",
+          800: "#FAFAFA",
+        },
+        white: {
+          900: "#FFFFFF",
+        },
+      },
     },
   },
   plugins: [],
