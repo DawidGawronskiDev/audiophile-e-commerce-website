1f529dbf63ad1d9fe159fcdaea7ffca67d3470e0
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2024-01-22 15:44:13 +0000

    Create Categories component

---
+++ src/components/shared/Categories/Categories.jsx
@@ -0,0 +1,35 @@
+import { ButtonShop } from "../Buttons";
+
+const Category = ({ name }) => {
+  return (
+    <li className="h-[165px] bg-grey-900 flex flex-col justify-center items-center relative rounded-lg gap-4">
+      <img
+        className="w-[150px] absolute -top-1/3"
+        src={`/assets/shared/desktop/image-category-thumbnail-${name}.png`}
+        alt=""
+      />
+      <div className="absolute bottom-6 grid items-center justify-items-center gap-4">
+        <h6>{name}</h6>
+        <ButtonShop text={"Shop"} />
+      </div>
+    </li>
+  );
+};
+
+const Categories = () => {
+  const categories = ["headphones", "speakers", "earphones"];
+
+  return (
+    <section>
+      <div className="container-center">
+        <ul className="grid gap-16 tablet:grid-cols-3 tablet:gap-4 desktop:gap-8">
+          {categories.map((category, index) => (
+            <Category name={category} key={index} />
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default Categories;
