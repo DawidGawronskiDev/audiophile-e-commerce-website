5549835a93ddba833000d110562cbcc0ed55783c
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2024-01-22 10:37:48 +0000

    Add screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html",
-  "./src/**/*.{js,ts,jsx,tsx}",],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        desktop: "1440px",
+        tablet: "768px",
+        mobile: "375px",
+      },
+    },
   },
   plugins: [],
-}
-
+};
