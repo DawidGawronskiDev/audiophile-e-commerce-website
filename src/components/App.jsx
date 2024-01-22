40877d95a25ae251596a7eb4234e2f583f2b7440
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2024-01-22 16:45:17 +0000

    Additional commit, save App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,13 @@
+import Header from "./Header/Header";
+import Categories from "./shared/Categories/Categories";
+
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <>
+      <Header />
+      <Categories />
+    </>
+  );
 }
 
-export default App
+export default App;
