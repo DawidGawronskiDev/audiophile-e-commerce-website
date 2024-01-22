d427d834fd4daa570b74697d8aa490e05025d15b
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2024-01-22 13:34:45 +0000

    Create Hero Component

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,23 @@
+import { ButtonOrange } from "../shared/Buttons";
+
+const Hero = () => {
+  return (
+    <section className="bg-black-900 h-[729px]">
+      <div
+        className={`container-center h-[100%] grid items-center justify-center desktop:justify-start bg-hero`}
+      >
+        <div className="max-w-[396px] grid gap-6 text-center desktop:text-left justify-items-center desktop:justify-items-start">
+          <span className="text-overline text-white-900/50">New product</span>
+          <h1 className="text-white-900">XX99 Mark II HeadphoneS</h1>
+          <span className="body text-white-900/75">
+            Experience natural, lifelike audio and exceptional build quality
+            made for the passionate music enthusiast.
+          </span>
+          <ButtonOrange text={"See Product"} />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
