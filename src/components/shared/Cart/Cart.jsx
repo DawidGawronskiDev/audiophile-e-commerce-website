d4d16e58f8dd54d6118374e072c6884bac2d93a3
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2024-01-24 09:48:29 +0000

    Update Cart styling

--- src/components/shared/Cart/Cart.jsx
+++ src/components/shared/Cart/Cart.jsx
@@ -63,40 +63,38 @@ const Cart = () => {
 
   return (
     <div className={`${cartVisible ? `block` : `hidden`}`}>
-      <div className="fixed z-30 top-0 left-0 bg-black-900/40 w-full h-full">
-        x
-      </div>
-      <div className="fixed z-40 rounded-lg bg-white-900 w-[90dvw] top-28 left-1/2 -translate-x-1/2 p-8 grid gap-6">
-        <div className="flex">
-          <h6 className="flex-1">
-            Cart (
-            {cartItems.reduce((acc, val) => {
-              acc += val.quantity;
-              return acc;
-            }, 0)}
-            )
-          </h6>
-          <button className="text-black-900/50">
-            <span>Remove All</span>
-          </button>
+      <div className="fixed z-50 top-0 left-1/2 -translate-x-1/2 container-center w-full h-[calc(100dvh_-_90px)] mt-[90px] tablet:grid items-start justify-items-end">
+        <div className="z-40 bg-grey-900 p-2 mobile:p-6 rounded-lg grid gap-8 my-8 tablet:w-[377px]">
+          <div className="flex">
+            <h6 className="flex-1">
+              Cart (
+              {cartItems.reduce((acc, val) => {
+                acc += val.quantity;
+                return acc;
+              }, 0)}
+              )
+            </h6>
+            <button className="text-black-900/50">Remove All</button>
+          </div>
+          <ul className="grid gap-6">
+            {cartItems.map((item) => (
+              <CartItem key={item.id} item={item} />
+            ))}
+          </ul>
+          <div className="flex">
+            <span className="flex-1 uppercase">Total</span>
+            <h6>
+              &#36;{" "}
+              {cartItems.reduce((acc, val) => {
+                acc += val.quantity * val.price;
+                return acc;
+              }, 0)}
+            </h6>
+          </div>
+          <ButtonOrange text={"Go to checkout"} />
         </div>
-        <ul className="grid gap-6">
-          {cartItems.map((item) => (
-            <CartItem key={item.id} item={item} />
-          ))}
-        </ul>
-        <div className="flex items-center">
-          <span className="flex-1 text-black-900/50">TOTAL</span>
-          <h6>
-            &#36;{" "}
-            {cartItems.reduce((acc, val) => {
-              acc += val.quantity * val.price;
-              return acc;
-            }, 0)}
-          </h6>
-        </div>
-        <ButtonOrange text={"Go to checkout"} />
       </div>
+      <div className="fixed z-40 top-0 left-0 bg-black-900/25 w-[100dvw] h-[100dvh]"></div>
     </div>
   );
 };
