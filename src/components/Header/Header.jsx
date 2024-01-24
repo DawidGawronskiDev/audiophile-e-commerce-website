3007b3169b53cbf2519f220cac4face849cbc20e
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2024-01-24 14:41:57 +0000

    Add navigation for mobile devices

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,12 @@
 import LogoImage from "/assets/shared/desktop/logo.svg";
 import CartImage from "/assets/shared/desktop/icon-cart.svg";
 
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../App";
+
+import HamburgerIcon from "/assets/shared/tablet/icon-hamburger.svg";
+import Categories from "../shared/Categories/Categories";
 
 const NavLink = ({ name, link }) => {
   return (
@@ -64,16 +67,42 @@ const CartButton = () => {
 };
 
 const Header = () => {
+  const location = useLocation();
+  const [menuVisible, setMenuVisible] = useState(false);
+
+  const handleMenuVisible = () => {
+    setMenuVisible(!menuVisible);
+  };
+
+  useEffect(() => {
+    setMenuVisible(false);
+  }, [location.pathname]);
+
   return (
-    <header className="fixed z-50 top-0 left-0 w-full bg-black-900">
-      <div className="container-center h-[90px] flex justify-between py-8">
-        <img src={LogoImage} alt="Audiophile Logo" />
-        <nav className="hidden tablet:block">
-          <NavLinks />
-        </nav>
-        <CartButton />
-      </div>
-    </header>
+    <>
+      <header className="fixed z-50 top-0 left-0 w-full bg-black-900">
+        <div className="container-center h-[90px] flex justify-between py-8">
+          <button className="tablet:hidden" onClick={handleMenuVisible}>
+            <img src={HamburgerIcon} alt="Menu" />
+          </button>
+          <Link to={"/"}>
+            <button>
+              <img src={LogoImage} alt="Audiophile Logo" />
+            </button>
+          </Link>
+          <nav className="hidden tablet:block">
+            <NavLinks />
+          </nav>
+          <CartButton />
+        </div>
+        <div className={`tablet:hidden ${menuVisible ? `block` : `hidden`}`}>
+          <div className="mt-[10px] pt-20 pb-8 bg-white-900">
+            <Categories />
+          </div>
+          <div className="fixed w-[100dvw] h-[100dvh] bg-black-900/25"></div>
+        </div>
+      </header>
+    </>
   );
 };
 
