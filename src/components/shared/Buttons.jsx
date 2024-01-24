76a892d07129f6503b1abd7d18e610bdbdb2bf3e
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2024-01-24 13:04:50 +0000

    Fix links in Buttons

--- src/components/shared/Buttons.jsx
+++ src/components/shared/Buttons.jsx
@@ -2,12 +2,14 @@ import { Link } from "react-router-dom";
 
 const ButtonOrange = ({ text, link }) => {
   return (
-    <button
-      style={defaultStyling}
-      className="text-white-900 bg-orange-900 hover:bg-orange-800 transition"
-    >
-      <Link to={link}>{text}</Link>
-    </button>
+    <Link to={link}>
+      <button
+        style={defaultStyling}
+        className="text-white-900 bg-orange-900 hover:bg-orange-800 transition"
+      >
+        {text}
+      </button>
+    </Link>
   );
 };
 
@@ -21,14 +23,16 @@ const defaultStyling = {
 
 const ButtonShop = ({ text, link }) => {
   return (
-    <button className="text-black-900/50 hover:text-orange-900 transition">
-      <Link className="flex items-center gap-2">
-        <span className="font-bold text-[13px] tracking-[1px] uppercase">
-          {text}
-        </span>
-        <img src="/assets/shared/desktop/icon-arrow-right.svg" alt="" />
-      </Link>
-    </button>
+    <Link to={link}>
+      <button className="text-black-900/50 hover:text-orange-900 transition">
+        <div className="flex items-center gap-2">
+          <span className="font-bold text-[13px] tracking-[1px] uppercase">
+            {text}
+          </span>
+          <img src="/assets/shared/desktop/icon-arrow-right.svg" alt="" />
+        </div>
+      </button>
+    </Link>
   );
 };
 
