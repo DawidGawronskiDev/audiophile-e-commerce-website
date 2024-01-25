17ad64afbac29a328bcbd3af80843b7f60658475
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2024-01-25 14:38:41 +0000

    Refactor Category

--- src/components/shared/Categories/Categories.jsx
+++ src/components/shared/Categories/Categories.jsx
@@ -1,20 +1,4 @@
-import { ButtonShop } from "../Buttons";
-
-const Category = ({ name }) => {
-  return (
-    <li className="h-[165px] bg-grey-900 flex flex-col justify-center items-center relative rounded-lg gap-4">
-      <img
-        className="w-[150px] absolute -top-1/3"
-        src={`/assets/shared/desktop/image-category-thumbnail-${name}.png`}
-        alt=""
-      />
-      <div className="absolute bottom-6 grid items-center justify-items-center gap-4">
-        <h6>{name}</h6>
-        <ButtonShop text={"Shop"} name={name} />
-      </div>
-    </li>
-  );
-};
+import Category from "./Category";
 
 const Categories = () => {
   const categories = ["headphones", "speakers", "earphones"];
