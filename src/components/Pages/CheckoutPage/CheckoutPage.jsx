40c5ce45648511615793bfa085226de587c226e8
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2024-01-25 14:04:22 +0000

    Update Modal styling

--- src/components/Pages/CheckoutPage/CheckoutPage.jsx
+++ src/components/Pages/CheckoutPage/CheckoutPage.jsx
@@ -335,7 +335,7 @@ const Modal = ({ modalOpen }) => {
 
   return (
     <div className={`${modalOpen ? `block` : `hidden`}`}>
-      <div className="fixed z-50 top-0 left-1/2 -translate-x-1/2 container-center w-full h-[calc(100dvh_-_90px)] mt-[90px] tablet:grid items-start justify-items-end">
+      <div className="fixed z-50 top-0 left-1/2 -translate-x-1/2 tablet:top-1/2 tablet:-translate-y-1/2 container-center w-full h-[calc(100dvh_-_90px)] tablet:grid tablet:items-center tablet:justify-items-center">
         <div className="z-40 bg-white-900 p-2 mobile:p-12 rounded-lg grid gap-8 my-8 tablet:w-[450px]">
           <img src={"/assets/checkout/icon-order-confirmation.svg"} alt="" />
           <h3>Thank You for your order</h3>
