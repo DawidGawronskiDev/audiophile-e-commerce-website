309e4d45e6f3d040e2f772f71466d9987a2ff063
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2024-01-25 14:29:50 +0000

    Refactor Cart

--- src/components/shared/Cart/Cart.jsx
+++ src/components/shared/Cart/Cart.jsx
@@ -3,60 +3,7 @@ import { useContext } from "react";
 import { ButtonOrange } from "../Buttons";
 import { ShopContext } from "../../App";
 
-const QuantityButton = ({ id, quantity }) => {
-  const { handleItemQuantity } = useContext(ShopContext);
-
-  return (
-    <div className="flex gap-4">
-      <div className="flex font-bold text-black-900/25 bg-grey-900 *:aspect-square *:text-[13px] *:w-8 *:flex *:items-center *:justify-center *:cursor-pointer">
-        <div
-          onClick={() => {
-            handleItemQuantity(-1, id);
-          }}
-        >
-          <span>-</span>
-        </div>
-        <div>
-          <span className="text-black-900">{quantity}</span>
-        </div>
-        <div
-          onClick={() => {
-            handleItemQuantity(1, id);
-          }}
-        >
-          <span>+</span>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-const CartItem = ({ item }) => {
-  const name = item.name
-    .split(" ")
-    .reduce((acc, val) => {
-      if (val.length < 5) {
-        acc.push(val);
-      }
-      return acc;
-    }, [])
-    .join(" ");
-
-  return (
-    <li className="flex items-center gap-4 text-[15px]">
-      <div className="w-16 aspect-square bg-grey-800">
-        <img src={item.image.mobile} alt="" className="rounded-lg " />
-      </div>
-      <div className="flex-1">
-        <h6 className="leading-[25px]">{name}</h6>
-        <span className="font-bold text-black-900/50 text-[14px] leading-[25px]">
-          &#36; {item.price}
-        </span>
-      </div>
-      <QuantityButton id={item.id} quantity={item.quantity} />
-    </li>
-  );
-};
+import CartItem from "./CartItem";
 
 const Cart = () => {
   const { cartItems, cartVisible } = useContext(ShopContext);
---
+++ src/components/shared/Cart/CartItem.jsx
@@ -0,0 +1,30 @@
+import QuantityButton from "./QuantityButton";
+
+const CartItem = ({ item }) => {
+  const name = item.name
+    .split(" ")
+    .reduce((acc, val) => {
+      if (val.length < 5) {
+        acc.push(val);
+      }
+      return acc;
+    }, [])
+    .join(" ");
+
+  return (
+    <li className="flex items-center gap-4 text-[15px]">
+      <div className="w-16 aspect-square bg-grey-800">
+        <img src={item.image.mobile} alt="" className="rounded-lg " />
+      </div>
+      <div className="flex-1">
+        <h6 className="leading-[25px]">{name}</h6>
+        <span className="font-bold text-black-900/50 text-[14px] leading-[25px]">
+          &#36; {item.price}
+        </span>
+      </div>
+      <QuantityButton id={item.id} quantity={item.quantity} />
+    </li>
+  );
+};
+
+export default CartItem;
