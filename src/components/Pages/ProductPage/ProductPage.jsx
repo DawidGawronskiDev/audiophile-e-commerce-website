e23448208102fa726fa69c1740683b25dcdcac25
DawidGawronskiDev <dawidgawronskicontact@gmail.com>
2024-01-26 21:39:37 +0000

    Update ProductPage rendering

--- src/components/Pages/ProductPage/ProductPage.jsx
+++ src/components/Pages/ProductPage/ProductPage.jsx
@@ -10,41 +10,53 @@ import Features from "./Features";
 import Content from "./Content";
 import Gallery from "./Gallery";
 import Others from "./Others";
+import { useEffect, useState } from "react";
 
-async function loader(params) {
-  const fetchData = async () => {
-    try {
-      const response = await fetch("/data.json");
-      const data = await response.json();
-
-      return { data, slug: params.params.slug };
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  return fetchData();
+async function loader(response) {
+  return response.params;
 }
 
 const ProductPage = () => {
   const loaderData = useLoaderData();
 
-  const product = loaderData.data.find(
-    (product) => product.slug === loaderData.slug
-  );
+  const [product, setProduct] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-  return (
-    <>
-      <div className="mt-[90px]"></div>
-      <Product product={product} />
-      <Features product={product} />
-      <Content product={product} />
-      <Gallery product={product} />
-      <Others product={product} />
-      <Categories />
-      <Testimonial />
-    </>
-  );
+  useEffect(() => {
+    fetch("/data.json")
+      .then((response) => {
+        if (response.status >= 400) {
+          throw new Error("server error");
+        }
+        return response.json();
+      })
+      .then((response) => {
+        const fetchedProduct = response.find(
+          (product) => product.slug === loaderData.slug
+        );
+        setProduct(fetchedProduct);
+      })
+      .catch((error) => setError(error))
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [loaderData.slug]);
+
+  if (product) {
+    return (
+      <>
+        <div className="mt-[90px]"></div>
+        <Product product={product} />
+        <Features product={product} />
+        <Content product={product} />
+        <Gallery product={product} />
+        <Others product={product} />
+        <Categories />
+        <Testimonial />
+      </>
+    );
+  }
 };
 
 export default ProductPage;
