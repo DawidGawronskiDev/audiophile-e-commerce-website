e0109da3a4276dc87f8a1e4fe356a612540453a1
DawidGawronskiDev <dawidgawronskicontact@gmail.com>
2024-04-29 20:48:18 +0100

    Start new project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {
-      screens: {
-        desktop: "1440px",
-        tablet: "768px",
-        mobile: "375px",
-      },
-      colors: {
-        orange: {
-          900: "#D87D4A",
-          800: "#fbaf85",
-        },
-        black: {
-          900: "#000000",
-          800: "#101010",
-        },
-        grey: {
-          900: "#F1F1F1",
-          800: "#FAFAFA",
-          700: "#4C4C4C",
-        },
-        white: {
-          900: "#FFFFFF",
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
+
