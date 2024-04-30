f8e1b31f02f551f14483d91d65d130663914919f
DawidGawronskiDev <dawidgawronskicontact@gmail.com>
2024-04-30 22:48:33 +0100

    Update Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,14 @@ import LogoImage from "../assets/shared/desktop/logo.svg";
 import CategoryList from "./CategoryList";
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { cartActions } from "../store/cartSlice";
 
 const Header = () => {
-  const location = useLocation();
   const [showMenu, setShowMenu] = useState(false);
+
+  const dispatch = useDispatch();
+  const location = useLocation();
 
   useEffect(() => {
     setShowMenu(false);
@@ -17,6 +21,10 @@ const Header = () => {
     setShowMenu(!showMenu);
   };
 
+  const openCart = () => {
+    dispatch(cartActions.showCart());
+  };
+
   return (
     <div className="fixed top-0 left-0 z-50 w-full bg-white">
       <header className="px-6 py-8 bg-black text-white flex items-center justify-between h-24">
@@ -24,7 +32,7 @@ const Header = () => {
           <img src={HamburgerIcon} alt="Menu" />
         </button>
         <img src={LogoImage} alt="Logo" />
-        <button>
+        <button onClick={openCart}>
           <img src={CartIcon} alt="Cart" />
         </button>
       </header>
