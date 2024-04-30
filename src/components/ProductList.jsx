53033344c3ea268647cb51a8b62ddcec5cfe0efd
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2024-04-30 10:07:15 +0100

    Update ProductList

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,6 +1,47 @@
+import { useEffect, useState } from "react";
 import ProductItem from "./ProductItem";
 
-const ProductList = ({ products }) => {
+const ProductList = ({ type }) => {
+  const [products, setProducts] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setIsLoading(true);
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch("src/data.json");
+
+        if (!response.ok) {
+          throw json({ message: "Failed to fetch products" });
+        }
+
+        const responseData = await response.json();
+
+        const products = responseData.filter(
+          (product) => product.category === type,
+        );
+
+        setProducts(products);
+      } catch (error) {
+        console.error(error);
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchProducts();
+  }, [type]);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>{error.message}</p>;
+  }
+
   return (
     <ul className="mx-6 grid gap-32">
       {products &&
--- src/pages/Headphones.jsx
+++ src/pages/Headphones.jsx
@@ -7,50 +7,10 @@ import ProductList from "../components/ProductList";
 import CategoryHero from "../components/CategoryHero";
 
 const HeadphonesPage = () => {
-  const [products, setProducts] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    setIsLoading(true);
-    const fetchProducts = async () => {
-      try {
-        const response = await fetch("src/data.json");
-
-        if (!response.ok) {
-          throw json({ message: "Failed to fetch products" });
-        }
-
-        const responseData = await response.json();
-
-        const products = responseData.filter(
-          (product) => product.category === "headphones",
-        );
-
-        setProducts(products);
-      } catch (error) {
-        console.error(error);
-        setError(error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    fetchProducts();
-  }, []);
-
-  if (isLoading) {
-    return <p>Loading...</p>;
-  }
-
-  if (error) {
-    return <p>{error.message}</p>;
-  }
-
   return (
     <>
       <CategoryHero>Headphones</CategoryHero>
-      <ProductList products={products} />
+      <ProductList type="headphones" />
       <CategoryList />
       <Testimonial />
       <Footer />
