9a2e3bf54a498d8b862a8bfab4c01a14155cdb0a
DawidGawronskiDev <dawidgawronskicontact@gmail.com>
2024-04-30 22:39:45 +0100

    Add reducers to cartSlice

--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -15,6 +15,21 @@ const cartSlice = createSlice({
     hideCart: (state) => {
       state.isCartVisible = false;
     },
+    addItem: (state, action) => {
+      const { item, quantity } = action.payload;
+
+      const newItem = item;
+      const existingItemIndex = state.items.findIndex(
+        (item) => item.id === newItem.id,
+      );
+
+      if (existingItemIndex !== -1) {
+        state.items[existingItemIndex].quantity =
+          state.items[existingItemIndex].quantity + quantity;
+      } else {
+        state.items.push({ ...newItem, quantity: quantity });
+      }
+    },
   },
 });
 
