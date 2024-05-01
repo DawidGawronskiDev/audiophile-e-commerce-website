e204595da04106a94725679a7fbe170a288e72f8
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2024-05-01 13:53:58 +0100

    Update CartList

--- src/components/CartList.jsx
+++ src/components/CartList.jsx
@@ -3,9 +3,11 @@ import CartItem from "./CartItem";
 const CartList = ({ items }) => {
   return (
     <ul className="flex flex-col gap-6">
-      {items.map((item) => (
-        <CartItem key={item.id} item={item} />
-      ))}
+      {items.length > 0 ? (
+        items.map((item) => <CartItem key={item.id} item={item} />)
+      ) : (
+        <span>Cart is empty.</span>
+      )}
     </ul>
   );
 };
