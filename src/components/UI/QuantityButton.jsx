9216173b722015db8a8100027d8158339de1b584
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2024-05-01 11:48:46 +0100

    Update QuantityButton styling

--- src/components/UI/QuantityButton.jsx
+++ src/components/UI/QuantityButton.jsx
@@ -1,6 +1,6 @@
 const QuantityButton = ({ itemQuantity, onSubstract, onAdd }) => {
   return (
-    <div className="flex *:aspect-square *:w-12 bg-gray-100 w-min">
+    <div className="flex *:aspect-square *:w-8 sm:*:w-12 bg-gray-100 w-min">
       <button onClick={onSubstract}>-</button>
       <div className="grid content-center text-center">
         <span>{itemQuantity}</span>
