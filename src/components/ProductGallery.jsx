3b227e45943e7c71af1baae06dbe4a6612b2aedf
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2024-05-01 13:55:09 +0100

    Update ProductGallery

---
+++ src/components/ProductGallery.jsx
@@ -0,0 +1,23 @@
+const ProductGallery = ({ gallery }) => {
+  console.log(Object.values(gallery));
+
+  return (
+    <div className="mx-6">
+      <ul className="grid gap-5">
+        {Object.values(gallery).map((item, index) => {
+          return (
+            <li key={index}>
+              <img
+                src={`http://localhost:3000${item.mobile}`}
+                alt=""
+                className="rounded-lg"
+              />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default ProductGallery;
